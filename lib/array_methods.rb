96cf1408e8ae0328eaf76565dc9a57899d2d899b
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-03 23:39:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
 def using_include(array, element)
+  array.include?(element)
 
 end
 
 def using_sort(array)
-
+  arrray.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
+  arrray.first
 
 end
 
 def using_last(array)
+  array.last
 
 end
 
 def using_size(array)
+  array.size
 
 end
